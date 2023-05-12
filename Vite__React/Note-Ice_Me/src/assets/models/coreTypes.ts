5e4d548b79904c4903e43a2edbfdc28f47634b97
Marco Ottina <m.ottina@reply.it>
2023-05-12 22:28:31 +0200

    preparing develop for Vite implementation
    Signed-off-by: Marco Ottina <m.ottina@reply.it>

---
+++ Vite__React/Note-Ice_Me/src/assets/models/coreTypes.ts
@@ -0,0 +1,79 @@
+/**
+ * Abstract node of the note-tree.
+ */
+export type NoteNodeAbstract = {
+  father?: NoteNodeAbstract;
+
+  /** must be non-negative */
+  depthLevel: number;
+};
+
+/**
+ * Enumeration of available, defined types.
+ */
+export enum TerminalTypes {
+  Text = 'text',
+  Uri = 'uri',
+  File = 'file',
+}
+
+/**
+ * Terminal node, may consists of a simple text or resource.
+ */
+export type TerminalNode = NoteNodeAbstract & {
+  type: TerminalTypes;
+};
+
+/**
+ * Utility type used just to define a non-abstract node
+ */
+export type ConcreteNode = SubInternalNode | TerminalNode;
+
+/**
+ * Plain text.
+ */
+export type TextNode = TerminalNode & {
+  text: string;
+};
+
+/**
+ * Just an URI.
+ * It's similar to a text node but can be used to specifically render.
+ * some link preview.
+ */
+export type LinkNode = TerminalNode & {
+  uri: string;
+};
+
+/**
+ * Any kind of file: textual, image, video, audio, etc.
+ */
+export type FileNode = TerminalNode & {
+  /**
+   * Identifier of the file stored inside a storage (folder, database, etc).
+   */
+  fileID: string;
+};
+
+/**
+ * An internal node on the tree, listing other nodes of the same single level.
+ */
+export type SubInternalNode = NoteNodeAbstract & {
+  subparts?: ConcreteNode[];
+};
+
+type NoteGeneral = {
+  mainText: string;
+  notesRoot?: ConcreteNode;
+  comments?: string;
+};
+
+export default NoteGeneral;
+
+
+//
+
+// FUNCTIONS
+
+//
+
